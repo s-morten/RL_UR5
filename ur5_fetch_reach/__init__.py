ccd9e54f8b06fd2102f1b31559f1cfe22bedbd2a
Morten Stehr <morten.stehr@haw-hamburg.de>
2020-09-22 16:57:15 +0200

    removed unnacassary files

--- ur5_fetch_reach/__init__.py
+++
@@ -1,6 +0,0 @@
-from ur5_fetch_reach.env.registration import register
-
-register(
-    id='UR5-v0',
-    entry_point='ur5_fetch_reach.env:UR5',
-)
--- ur5_fetch_reach/env/__init__.py
+++
@@ -1,1 +0,0 @@
-from ur5_fetch_reach.env.fetch_reach_env import UR5
