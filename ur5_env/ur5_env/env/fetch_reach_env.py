f82ee415a7df4d86370936ed640a84a78ec743e6
Morten Stehr <morten.stehr@haw-hamburg.de>
2020-09-24 21:06:45 +0200

    adding missing deps

--- ur5_env/ur5_env/env/fetch_reach_env.py
+++ ur5_env/ur5_env/env/fetch_reach_env.py
@@ -19,7 +19,7 @@ class UR5(FetchEnv, utils.EzPickle):
             'robot0:slide1': 0.48,
             'robot0:slide2': 0.0,
         }
-        fetch_env.FetchEnv.__init__(
+        FetchEnv.__init__(
             self, MODEL_XML_PATH, has_object=False, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
