13e4c8c3526b9f75fcc3a4f593614e72f0a745c0
Morten Stehr <morten.stehr@haw-hamburg.de>
2020-09-25 14:23:21 +0200

    stole Mujoco controller from Github

--- ur5_env/ur5_env/env/fetch_reach_env.py
+++ ur5_env/ur5_env/env/fetch_reach_env.py
@@ -3,12 +3,7 @@ import numpy as np
 from gym import utils, spaces
 from gym.envs.mujoco import mujoco_env
 from collections import defaultdict
-import sys
-sys.path.append('/home/morten/.local/lib/python3.6/site-packages/gym/envs/robotics')
-from fetch_env import FetchEnv
-
-# from env.robotics import fetch_env
-
+from env.mujoco_controller import MJ_Controller
 
 # Ensure we get the path separator correct on windows
 MODEL_XML_PATH = '/home/morten/RL_husky/ur5_env/ur5_env/env/xml/UR5gripper_2_finger.xml'
@@ -16,26 +11,65 @@ MODEL_XML_PATH = '/home/morten/RL_husky/ur5_env/ur5_env/env/xml/UR5gripper_2_fin
 
 
 class UR5(mujoco_env.MujocoEnv, utils.EzPickle):
-    def __init__(self, reward_type='sparse', render=True):
+    def __init__(self, reward_type='sparse', render=True, image_width=200, image_height=200):
+        self.initialized = False
+        self.IMAGE_WIDTH = image_width
+        self.IMAGE_HEIGHT = image_height
+        self.rotations = {0: 0, 1: 30, 2: 60, 3: 90, 4: -30, 5: -60}
+        utils.EzPickle.__init__(self)
         mujoco_env.MujocoEnv.__init__(self, MODEL_XML_PATH, 1)
         if render:
             self.render()
+        self.controller = MJ_Controller(self.model, self.sim, self.viewer)
+        self.initialized = True
         self.render=render
-        utils.EzPickle.__init__(self)
 
     def _set_action_space(self):
-        self.action_space = spaces.Discrete(6)
+        self.action_space = spaces.MultiDiscrete([self.IMAGE_HEIGHT*self.IMAGE_WIDTH, len(self.rotations)])
         return self.action_space
 
     def step(self, action):
-        print(action)
-        self.obs = defaultdict()
-        self.obs['obs'] = np.zeros((5,3,6))
-        return self.obs, 0, 0, 0
+        done = False
+        info = {}
 
+        if not self.initialized:
+            # self.current_observation = np.zeros((200,200,4))
+            self.current_observation = defaultdict()
+            self.current_observation['rgb'] = np.zeros((self.IMAGE_WIDTH,self.IMAGE_HEIGHT,3))
+            self.current_observation['depth'] = np.zeros((self.IMAGE_WIDTH,self.IMAGE_HEIGHT))
+            reward = 0
+        else:
+            x = action[0] % self.IMAGE_WIDTH
+            y = action[0] // self.IMAGE_WIDTH
+            rotation = action[1]
+
+            depth = self.current_observation['depth'][y][x]
+
+            coordinates = self.controller.pixel_2_world(pixel_x=x, pixel_y=y, depth=depth, height=self.IMAGE_HEIGHT, width=self.IMAGE_WIDTH)
+
+            result = self.controller.move_ee(coordinates, max_steps=1000, quiet=True, render=render, marker=markers, tolerance=0.05, plot=plot)
+
+            self.current_observation = self.get_observation(show=self.show_observations)
+
+        return self.current_observation, reward, done, info
 
     def reset():
         return 0, 0, 0, 0
 
-    def do_render(self):
-        self.viewer.render()
+    #def do_render(self):
+    #    self.viewer.render()
+
+    def get_observation(self, show=True):
+        """
+        Uses the controllers get_image_data method to return an top-down image (as a np-array).
+        Args:
+            show: If True, displays the observation in a cv2 window.
+        """
+
+        rgb, depth = self.controller.get_image_data(width=self.IMAGE_WIDTH, height=self.IMAGE_HEIGHT, show=show)
+        depth = self.controller.depth_2_meters(depth)
+        observation = defaultdict()
+        observation['rgb'] = rgb
+        observation['depth'] = depth
+
+        return observation
