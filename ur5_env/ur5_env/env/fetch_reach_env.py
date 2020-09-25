119d550c1f7d05587eacbc5cf0958c83adcd54a9
Morten Stehr <mortenstehr@hotmail.de>
2020-09-25 13:19:35 +0200

    changed whole own env

--- ur5_env/ur5_env/env/fetch_reach_env.py
+++ ur5_env/ur5_env/env/fetch_reach_env.py
@@ -1,5 +1,8 @@
 import os
-from gym import utils
+import numpy as np
+from gym import utils, spaces
+from gym.envs.mujoco import mujoco_env
+from collections import defaultdict
 import sys
 sys.path.append('/home/morten/.local/lib/python3.6/site-packages/gym/envs/robotics')
 from fetch_env import FetchEnv
@@ -8,20 +11,31 @@ from fetch_env import FetchEnv
 
 
 # Ensure we get the path separator correct on windows
-MODEL_XML_PATH = '/home/morten/RL_husky/ur5_env/ur5_env/env/xml/reach.xml'
+MODEL_XML_PATH = '/home/morten/RL_husky/ur5_env/ur5_env/env/xml/UR5gripper_2_finger.xml'
 
 
 
-class UR5(FetchEnv, utils.EzPickle):
-    def __init__(self, reward_type='sparse'):
-        initial_qpos = {
-            'robot0:slide0': 0.4049,
-            'robot0:slide1': 0.48,
-            'robot0:slide2': 0.0,
-        }
-        FetchEnv.__init__(
-            self, MODEL_XML_PATH, has_object=False, block_gripper=True, n_substeps=20,
-            gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
-            obj_range=0.15, target_range=0.15, distance_threshold=0.05,
-            initial_qpos=initial_qpos, reward_type=reward_type)
+class UR5(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self, reward_type='sparse', render=True):
+        mujoco_env.MujocoEnv.__init__(self, MODEL_XML_PATH, 1)
+        if render:
+            self.render()
+        self.render=render
         utils.EzPickle.__init__(self)
+
+    def _set_action_space(self):
+        self.action_space = spaces.Discrete(6)
+        return self.action_space
+
+    def step(self, action):
+        print(action)
+        self.obs = defaultdict()
+        self.obs['obs'] = np.zeros((5,3,6))
+        return self.obs, 0, 0, 0
+
+
+    def reset():
+        return 0, 0, 0, 0
+
+    def do_render(self):
+        self.viewer.render()
