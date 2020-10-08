aa687db0d357ad09bc76447103981728dd32baaf
Morten Stehr <morten.stehr@haw-hamburg.de>
2020-10-08 14:58:42 +0200

    new example

--- ur5_env/ur5_env/example3.py
+++ ur5_env/ur5_env/example3.py
@@ -1,32 +1,30 @@
 from ur5_env.env.mujoco_controller import MJ_Controller
+import random
 
+
+def get_action():
+    low = np.array([-3.14159, -3.14159, -3.14159, -3.14159, -3.14159], dtype=np.float32)
+    high = np.array([+3.14159,0,+3.14159,+3.14159,+3.14159], dtype=np.float32)
+    action = np.zeros(5, dtype=np.float32)
+
+    for i in range(len(low)):
+        action[i] = random.uniform(low[i], high[i])
+
+    return action
+
+
+STEPS = 100
 # create controller instance
 controller = MJ_Controller()
 
 # Display robot information
 controller.show_model_info()
 
-# Move ee to position above the object, plot the trajectory to an image file, show a marker at the target location
-controller.move_ee([0.0, -0.6 , 0.95], plot=True, marker=True)
-
-# Move down to object
-controller.move_ee([0.0, -0.6 , 0.895])
-
-# Wait a second
-controller.stay(1000)
-
-# Attempt grasp
-controller.grasp()
-
-# Move up again
-controller.move_ee([0.0, -0.6 , 1.0])
-
-# Throw the object away
-controller.toss_it_from_the_ellbow()
+for s in range(STEPS):
+    actiuon = get_action()
+    # Move ee to position above the object, plot the trajectory to an image file, show a marker at the target location
+    controller.move_group_to_joint_target(group='Arm', target=action, max_steps=1000, quiet=True, render=True, marker=False, tolerance=0.05, plot=False)
 
 
 # Wait before finishing
 controller.stay(2000)
-
-def get_action():
-    low[]
