292410b410cc4b2786f22033ae5ef8fe103ba570
Morten Stehr <morten.stehr@haw-hamburg.de>
2020-11-30 16:54:03 +0100

    working diff of achieved goal and wanted goal

--- ur5_env/ur5_env/example.py
+++ ur5_env/ur5_env/example.py
@@ -18,12 +18,7 @@ N_STEPS = 200
 env.print_info()
 
 for episode in range(1, N_EPISODES+1):
-    obs = env.reset(show_obs=SHOW_OBS)
-    print("++++++++++++++++++++++++++++++++++++++++++++")
-    print("++++++++++++++++++++++++++++++++++++++++++++")
-    print("reset")
-    print("++++++++++++++++++++++++++++++++++++++++++++")
-    print("++++++++++++++++++++++++++++++++++++++++++++")
+    env.reset()
     for step in range(N_STEPS):
         print('#################################################################')
         print(colored('EPISODE {} STEP {}'.format(episode, step+1), color='white', attrs=['bold']))
@@ -35,11 +30,12 @@ for episode in range(1, N_EPISODES+1):
         # -3.14 / 0
         #
         action = env.action_space.sample()
-        print(action)
         # action = [100,100] # multidiscrete
         # action = 20000 #discrete
-        #observation, reward, done, _ = env.step(action, record_grasps=True)
+        # observation, reward, done, _ = env.step(action, record_grasps=True)
         observation, reward, done, _ = env.step(action)
+        if done:
+            break
         # observation, reward, done, _ = env.step(action, record_grasps=True, render=True)
         # obs = env.reset(show_obs=SHOW_OBS)
 env.plot_actions()
